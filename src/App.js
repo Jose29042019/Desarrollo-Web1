a78415c011ad34d478e97dca37fac6827f96c8ac
Jose <naranjoalgarinj@gmail.com>
2023-08-24 11:11:57 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Prueba Jose Naranjo
         </p>
         <a
           className="App-link"
